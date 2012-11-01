6eb4ffb0140d43ac14928b3f5ce63e2f23119376
Christian Leskowsky <christian@leskowsky.net>
2012-11-01 16:24:45 -0400

    generified streampub dir

---
+++ cookbooks/application/recipes/default.rb
@@ -0,0 +1,6 @@
+
+web_app "app" do
+  docroot "/vagrant/git/app/..."
+  server_name "app"
+  server_aliases ["www.app"]
+end
--- cookbooks/streampub/recipes/default.rb
+++
@@ -1,6 +0,0 @@
-
-web_app "streampub" do
-  docroot "/vagrant/git/streampub/public"
-  server_name "streampub"
-  server_aliases ["www.streampub"]
-end
